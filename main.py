4e362590bd140e8576cc17e1908a52a48de3617a
Harsh Vyas <harshvyas5527+github@gmail.com>
2024-06-09 15:22:16 +0530

    Adding the count in input tile how much correct states are guessed by user. And also add the exit input for end the game. At the end of the game we create the csv file where we are store the data of cities which are unguessable by user.

--- main.py
+++ main.py
@@ -15,6 +15,15 @@ guess_states = []
 while len(guess_states) < 50:
     answer_state = screen.textinput(title=f"{len(guess_states)}/50 States Correct", prompt="What's another state's name?").title()
     
+    
+    if answer_state == "Exit":
+        missing_state =[]
+        for state in all_states:
+            if state not in guess_states:
+                missing_state.append(state)
+        new_csv = pandas.DataFrame(missing_state)
+        new_csv.to_csv("missing_state.csv")
+        break
     if answer_state in all_states:
         guess_states.append(answer_state)
         t = turtle.Turtle()
@@ -25,4 +34,4 @@ while len(guess_states) < 50:
         t.write(answer_state)
 
 
-screen.mainloop()
+
